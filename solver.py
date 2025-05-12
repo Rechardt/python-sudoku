30a71dc313e55365ac637694a00a0fb1d336b436
Rechardt Conradie <z5456894@ad.unsw.edu.au>
2025-05-12 11:53:32 +1000

    Added file reading

--- solver.py
+++ solver.py
@@ -2,17 +2,42 @@
 Reads a sudoku in from a file and then solves it using a recursive backtracker
 """
 import sys
+
+def read_file():
+    """
+    Read in the file, generating the list representation
+    The list representation is a 2D Array of 9x9
+    """
+    board = []
+    file_path = sys.argv[1]
+    with open(file_path, encoding="utf-8") as file:
+        for line in file.readlines():
+            row = []
+            rows = line.split("|")
+            if len(rows) == 1:
+                continue
+
+            for cell in rows:
+                for digit in cell:
+                    digit = digit.strip()
+                    if digit and digit != '.':
+                        row.append(int(digit))
+                    else:
+                        row.append(0)
+        
+            board.append(row[:-1]) # Remove the new line that was added as a 0
+    
+    return board
+
 def main():
     """
     Main progarm skeleton
     """
-    # Read in the file
-    file_path = sys.argv[1]
-    with open(file_path, encoding="utf-8") as file:
-        for line in file.readlines():
-            print(line)
+    board = read_file()
 
-    print("Hello World!")
+    # Print the 1st index in the second cell
+    print(board[0][0])
+
 
 if __name__ == "__main__":
     main()
